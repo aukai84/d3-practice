781359ba574b8c8c7bb212d06a0fea011e0016ef
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-04 21:43:43 -1000

    fill working

--- js/newApp.js
+++ js/newApp.js
@@ -1,4 +1,3 @@
-console.log("hellow orld")
 
 let container = d3.select('.container')
     .append('svg')
@@ -11,9 +10,24 @@ d3.json("https://d3js.org/us-10m.v1.json", (error, us) => {
     if(error) throw error;
 
     container.append('g')
-        .attr('class', 'counties')
-    .selectAll('path')
-    .data(topojson.feature(us, us.objects.counties).features)
+        .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
     .enter().append('path')
         .attr('d', path);
-})
+    console.log(us.objects.states)
+    container.append("path")
+        .attr("path", "state-borders")
+        .attr('d', path(topojson.mesh(us, us.objects.states, (a,b) => (a !== b))));
+
+    // container.append('g')
+    //     .attr("class", "counties")
+    // .selectAll("path")
+    // .data(topojson.feature(us, us.objects.counties).features)
+    // .enter().append('path')
+    //     .attr('d', path);
+
+    // container.append("path")
+    //     .attr("path", "county-borders")
+    //     .attr('d', path(topojson.mesh(us, us.objects.counties, (a,b) => (a !== b))));
+});
