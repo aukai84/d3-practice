6826a0f9ad6ccd4ab2230973a555e482e7869ae9
Aukai Tirrell <mtirrell@hawaii.edu>
2017-04-04 22:53:58 -1000

    shitty shopper lol

--- js/newApp.js
+++ js/newApp.js
@@ -12,13 +12,13 @@ d3.json("https://d3js.org/us-10m.v1.json", (error, us) => {
     container.append('g')
         .attr("class", "states")
     .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
+    .data(topojson.feature(us, us.objects.counties).features)
     .enter().append('path')
         .attr('d', path);
     console.log(us.objects.states)
     container.append("path")
         .attr("path", "state-borders")
-        .attr('d', path(topojson.mesh(us, us.objects.states, (a,b) => (a !== b))));
+        .attr('d', path(topojson.mesh(us, us.objects.counties, (a,b) => (a !== b))));
 
     // container.append('g')
     //     .attr("class", "counties")
